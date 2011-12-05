bb7ade436162b68577f9a6bf07b15bf8c0850e6b
hensley <papirazi14@hotmail.com>
2011-12-05 01:54:10 -0400

    modificacion de prueba

--- sistemaDelivery/Form1.Designer.cs
+++ sistemaDelivery/Form1.Designer.cs
@@ -38,7 +38,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(136, 94);
+            this.button1.Location = new System.Drawing.Point(81, 94);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(63, 23);
             this.button1.TabIndex = 0;
@@ -80,7 +80,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(205, 94);
+            this.button2.Location = new System.Drawing.Point(226, 94);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(64, 23);
             this.button2.TabIndex = 5;
